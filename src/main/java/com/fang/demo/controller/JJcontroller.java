c15a5e0a33f411470697a2d1eca02c4cf125f826
领哥哥 <fangsl@seehoo.cn>
2021-04-10 18:56:46 +0800

    ff

---
+++ src/main/java/com/fang/demo/controller/JJcontroller.java
@@ -0,0 +1,11 @@
+package com.fang.demo.controller;
+
+/**
+ * @ClassName JJcontroller
+ * @Author fang
+ * @Date 2021/4/10
+ * @Description TODO
+ * @Version 1.0
+ */
+public class JJcontroller {
+}
