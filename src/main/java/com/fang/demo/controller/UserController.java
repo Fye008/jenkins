a7e2d1d40be29c604eb8f250d48f13ba5fdaf303
领哥哥 <fangsl@seehoo.cn>
2021-04-12 09:54:54 +0800

    测试

--- src/main/java/com/fang/demo/controller/UserController.java
+++ src/main/java/com/fang/demo/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.fang.demo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,27 +19,8 @@ public class UserController {
     @PostMapping("/hello3")
     public String test1() {
 
-        System.out.println("进来了.............................................");
-
         return "hello world。。。";
     }
 
-
-    @GetMapping("/hello2")
-    public String test2() {
-
-        System.out.println("进来了.............................................");
-
-        return "hello world。。。";
-    }
-
-
-    @GetMapping("/hello4")
-    public String test4() {
-
-        System.out.println("进来了1111git00 ............................................000.");
-
-        return "hello world。。。";
-    }
 
 }
